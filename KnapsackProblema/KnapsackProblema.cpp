6f7943781035d833771be1c0f57047beaee2ee50
AdanVitor <adanvitor90@gmail.com>
2015-08-29 12:48:22 -0300

    First no parallel version

---
+++ KnapsackProblema/KnapsackProblema.cpp
@@ -0,0 +1,65 @@
+// KnapsackProblema.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "stdlib.h"
+#include "Math.h"
+#include "windows.h"
+#include "time.h"
+#include "stdio.h"
+#include "Headers.h"
+
+
+
+void generateTestCase( ){
+	
+testCase1();
+printf("\nTerminou teste Case 1");
+testCase2();
+printf("\nTerminou teste Case 2");
+testCase3();
+printf("\nTerminou teste Case 3");
+
+getchar();
+
+}
+
+
+void testOpenMP(){
+	// This statement should only print once
+	printf("Starting Program!\n");
+
+#pragma omp parallel
+	{
+		// This statement will run on each thread.
+		// If there are 4 threads, this will execute 4 times in total
+		printf("Running on multiple threads\n");
+	}
+
+	// We're out of the parallelized secion.
+	// Therefor, this should execute only once
+	printf("Finished!\n");
+}
+
+
+void main(){
+
+	testOpenMP();
+	
+	boolean testPdfOn = true;
+	if (!testPdfOn){
+		generateTestCase();
+	}
+	else{
+		imprimeResultado = true;
+		simpleTestCase();
+	}
+	
+
+}
+
+
+
+
+
+
